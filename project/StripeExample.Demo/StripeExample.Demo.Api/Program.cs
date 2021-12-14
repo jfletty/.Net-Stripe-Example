c0e375a7dd0ccefea09acc87435126ab6026467f
Josh Fletcher <fletcher.1996@hotmail.com>
2021-12-14 20:32:04 +1300

    Add quick DI to ensure services are referenced

--- project/StripeExample.Demo/StripeExample.Demo.Api/Program.cs
+++ project/StripeExample.Demo/StripeExample.Demo.Api/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RestSharp;
+using StripeExample.Demo.Services.Services;
+using StripeExample.Demo.Services.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +12,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IRestClient, RestClient>();
+builder.Services.AddScoped<IManageSubscription, ManageSubscription>();
+builder.Services.AddScoped<IManageCustomer, ManageCustomer>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
